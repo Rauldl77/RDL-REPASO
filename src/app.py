84289a3deda856e7f3e597d4dea1e7b3d3405a47
Raul <rdialeordialeo@gmail.com>
2024-05-19 14:59:46 +0200

    PAra que action funcione

--- src/app.py
+++ src/app.py
@@ -10,7 +10,6 @@ def read_root():
     return {"status": "ok-version1"}
 
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
